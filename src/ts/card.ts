091face46034f1816e6bb9e7bbe969f04f5c4052
Vinícius Neves Costa <vinicius.nevesco@gmail.com>
2022-07-07 21:23:00 -0300

    :construction: in progress
    Mobile layout ok.

--- src/ts/card.ts
+++ src/ts/card.ts
@@ -1,17 +1,13 @@
 export class Card {
-  title: string
-  desc: string
-  demo: string
-  repo: string
+  readonly title: string
+  readonly desc: string
+  readonly demo: string
+  readonly repo: string
 
   constructor(title: string, desc: string, demo: string, repo: string) {
     this.title = title;
     this.desc = desc;
     this.demo = demo
     this.repo = repo;
-  }
-
-  showTitle() {
-    return this.title;
   }
 }
